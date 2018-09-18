4f6e923eb9d446dad0fe5d64d7c38abf91d5234a
Nick Hance <nhance@reenhanced.com>
2018-09-18 16:48:01 -0400

    Initial commit, proven to work in V9 Dynamics 365

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import config from './config';
+
+export function OnLoad() {
+  // Locate Xrm fields for cities
+  // Enable autocomplete
+  // After selection, populate city details into mapped fields
+}
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  target: 'web',
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    // The library string below is how this will be exposed to CRM
+    // You can access any exported function from index.ts like this:
+    // `javascriptForCrm.OnLoad` which can be used for things like form script events
+    library: 'javascriptForCrm',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
