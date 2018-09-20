53598b172057c91bb1bc040d4913dca7a0ee133e
Nick Hance <nhance@reenhanced.com>
2018-09-20 10:06:28 -0400

    Autocomplete WIP. Savepoint to remove config file from repo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
 import config from './config';
+import { search } from './autocomplete';
+
+var OnKeypress = function(ext: Xrm.Events.EventContext) {
+  const control = ext.getEventSource() as Xrm.Controls.AutoLookupControl;
+  const query   = control.getValue();
+
+  let resultSet: Xrm.Controls.AutoCompleteResultSet = { results: [] };
+
+  if (query.length >= 3) {
+    search(query).then((response) => {
+      console.log(response);
+    })
+  }
+
+
+  control.showAutoComplete(resultSet)
+};
 
 export function OnLoad() {
   // Locate Xrm fields for cities
+  const fields: string[] = config.cityFields;
+
+  fields.forEach(fieldName => {
+    let autolookupControl = Xrm.Page.getControl<Xrm.Controls.AutoLookupControl>(fieldName);
+
+    autolookupControl.addOnKeyPress(OnKeypress);
+  });
   // Enable autocomplete
   // After selection, populate city details into mapped fields
 }
